a5d0e5734ae06dba1035517f67ea8664cf2782ff
jcervantes91016 <97720767+jcervantes91016@users.noreply.github.com>
2022-02-04 13:00:38 -0800

    Create 02.04.2022.java

---
+++ 02.04.2022.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      
+       Scanner kbrd= new Scanner(System.in);
+       for(int i=1; kbrd.hasNext(); i++)
+       {
+        
+           System.out.println(i + " " + kbrd.nextLine());
+       }
+    }
+}
