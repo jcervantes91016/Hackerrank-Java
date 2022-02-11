91d3c53b8d52632f156bc053738e65501121e17d
jcervantes91016 <97720767+jcervantes91016@users.noreply.github.com>
2022-02-11 14:09:42 -0800

    Create 2.9.22.java

---
+++ 2.9.22.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner kbrd= new Scanner(System.in);
+        for (int i = 1; kbrd.hasNextLine(); i++)
+        {
+            System.out.println(i + " " + kbrd.nextLine() );
+        }
+        }
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
